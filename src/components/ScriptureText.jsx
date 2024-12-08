b7b533bb2c4c334679bb449ab1ff759d368157c2
katiepo456 <kathryn.james@mymail.champlain.edu>
2024-12-07 22:09:17 -0500

    visual updates

--- src/components/ScriptureText.jsx
+++ src/components/ScriptureText.jsx
@@ -7,7 +7,7 @@ function ScriptureText({chapterData}) {
     <div id={`Chapter-${chapter}`} style={{ paddingTop: '5px' }}>
       <h2>{`CHAPTER ${chapter}`}</h2>
         {chapterData.map((verse, index) => (
-          <div key={index} style={{marginBottom: '5px'}}>
+          <div className="verses" key={index} style={{marginBottom: '5px'}}>
             <p>
               <strong>{`${verse.verse} `}</strong>{verse.text}
             </p>
